265d047c25a9e017fb621da9be43432bf1425cfc
wasabina67 <wasabina67@gmail.com>
2024-12-30 01:48:42 +0900

    update

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,10 +28,10 @@ Partial Class Form1
         'LabelTime
         '
         Me.LabelTime.AutoSize = True
-        Me.LabelTime.Font = New System.Drawing.Font("MS UI Gothic", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.LabelTime.Font = New System.Drawing.Font("MS UI Gothic", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.LabelTime.Location = New System.Drawing.Point(12, 9)
         Me.LabelTime.Name = "LabelTime"
-        Me.LabelTime.Size = New System.Drawing.Size(184, 48)
+        Me.LabelTime.Size = New System.Drawing.Size(245, 64)
         Me.LabelTime.TabIndex = 0
         Me.LabelTime.Text = "00:00:00"
         '
