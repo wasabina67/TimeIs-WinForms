a3a245f8987c1b84367e87b22ade8e7add7113ce
wasabina67 <wasabina67@gmail.com>
2024-12-30 01:29:49 +0900

    update

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Text = "TimeIs-WinForms"
+    End Sub
 End Class
