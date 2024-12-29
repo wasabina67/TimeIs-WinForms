97810626ad6944b1c923e7e5828cfb04cb20dd29
wasabina67 <wasabina67@gmail.com>
2024-12-30 01:44:45 +0900

    update

--- Form1.vb
+++ Form1.vb
@@ -1,7 +1,12 @@
 ﻿Public Class Form1
+    Private t As Timer
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.FormBorderStyle = FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Text = "TimeIs-WinForms"
+
+        t = New Timer()
     End Sub
+
 End Class
